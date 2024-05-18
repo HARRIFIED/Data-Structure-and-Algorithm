25e4ee6a25b5c5684b34b06a815ad791d51c5f11
HP <harrivic400@gmail.com>
2024-05-19 00:29:21 +0100

    Add twoSum solution

---
+++ Arrays/twoSum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target && i !== j) {
+                return [i, j]
+            }
+        }
+    }
+};
+
+//testing
+console.log(twoSum([1, 2, 3, 4, 5], 7))
+
+
