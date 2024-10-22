51d5d9c1940ef88cea13298d04f1538f081df4a5
HARRIFIED <harrivic400@gmail.com>
2024-10-22 09:27:16 +0100

    update

---
+++ Arrays/removeElement.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {*[]}
+ */
+var removeElement = function(nums, val) {
+    let modified = []
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+
+        }
+    }
+    console.log(modified)
+};
+
+removeElement([3,2,2,3], 3)
