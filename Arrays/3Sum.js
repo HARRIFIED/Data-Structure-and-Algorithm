f1a6c1845ebb092976247c6582053169c9bf6256
HARRIFIED <harrivic400@gmail.com>
2025-07-18 00:42:17 +0100

    Add 3Sum

---
+++ Arrays/3Sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    let triples = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+            if (sum === 0) {
+                triples.push([nums[i], nums[left], nums[right]]);
+
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+    return triples;
+};
+
+console.log(threeSum([7,-10,7,3,14,3,-2,-15,7,-1,-7,6,-5,-1,3,-13,6,-15,-10,14,8,5,-10,-1,1,1,11,6,8,5,-4,0,3,10,-12,-6,-2,-6,-6,-10,8,-5,12,10,1,-8,4,-8,-8,2,-9,-15,14,-11,-1,-8,5,-13,14,-2,0,-13,14,-12,12,-13,-3,-13,-12,-2,-15,4,8,4,-1,-6,11,11,-7,-12,-2,-8,10,-3,-4,-6,4,-14,-12,-5,0,3,-3,-9,-2,-6,-15,2,-11,-11,8,-11,8,-7,8,14,-5,4,10,3,-1,-15,10,-6,-11,13,-5,1,-15]));
--- Arrays/longestCommonPrefix.js
+++ Arrays/longestCommonPrefix.js
@@ -5,22 +5,17 @@
 
 //Horizontal scanning
 function longestCommonPrefix(strs) {
-    if (strs.length == 0) return "";
-    const firstString = strs[0]
-    let prefix = ""
-
-    for (let i = 0; i < firstString.length; i++) {
-        const char = firstString[i];
+    if (strs.length < 1) return "";
+    let first = strs[0];
+    for (let i = 0; i < first.length; i++) {
         for (let j = 1; j < strs.length; j++) {
-            if (i > strs[j].length || strs[j][i] !== char) {
-                return prefix
+            if (i > strs[j].length || strs[j][i] !== first[i]) {
+                return first.substring(0, i)
             }
         }
-        prefix += char
     }
-
-    return prefix
-}
+    return first
+} 
 
 const strings = ["floatation", "flow", "floating"]
 
