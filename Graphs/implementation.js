3777e1b908b92c43f094ea6b3815e1cd17544d00
HARRIFIED <harrivic400@gmail.com>
2025-10-10 03:39:40 +0100

    update

---
+++ Graphs/implementation.js
@@ -0,0 +1,33 @@
+/**
+ *                 2 ------ 0
+ *                /\
+ *               /  \
+ *              /    \
+ *             1      3
+ */
+
+// Edge List
+const graph = [[2, 0], [2, 1], [2, 3], [1, 3]]
+
+//* Adjacent List -- the index is the node and value is the node neighbour
+// It can be implemented with an array or linkedlist or objects
+//              0     1      2        3
+const graph2 = [[2], [2,3], [0,1,3], [1,2]]
+
+//* Adjacent Matrix --- using 0s and 1s to show if node x have a connection to node y
+// 0 - no 1 - yes. You could also use weighted value tooo
+const graph3 = [
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [1, 1, 0, 1],
+    [0, 1, 1, 0]
+]
+
+// or 
+const graph4 = {
+    0: [0, 0, 1, 0],
+    1: [0, 0, 1, 1],
+    2: [1, 1, 0, 1],
+    3: [0, 1, 1, 0]
+}
+
